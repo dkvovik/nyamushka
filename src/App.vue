e06c03031ca6c9e6bbd09a8490503c0c83055981
dkvovik <>
2018-07-09 11:19:04 +0300

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row content-wrapper align-items-center justify-content-center">
+        <div class="xs-12 md-6 md-4">
+          <CardProduct/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardProduct from './components/CardProduct'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CardProduct
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.content-wrapper {
+  height: 100vh;
 }
 </style>
